e3d7ea9cbeb264265b480db65903011ce5f783e0
TonyKelly12 <tonykelly1286@gmail.com>
2023-06-10 12:30:55 -0500

    rmv

--- Auth/GPT_Auth.py
+++ Auth/GPT_Auth.py
@@ -4,7 +4,7 @@ import openai
 def getGPTTokens():
     # openai.organization = os.getenv('gptOrgId')
     # openai.api_key = os.getenv("chatGPTkey")
-    openai.organization = 'org-6wpeAfQvGl3akIgcIDepYjxB'
-    openai.api_key = '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'
+    openai.organization = ''
+    openai.api_key = ''
     openai.Model.list()
 
